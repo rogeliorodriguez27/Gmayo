03d5478a769fa62252a8df8ded94b950883bc6da
rogelio <rogelio23rodriguez@gmail.com>
2022-02-01 19:56:23 -0400

    make it better

--- gmayo/settings.py
+++ gmayo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!6n_8%j$ga$bawln!i3cn4(&vfcyzy+5ek4=pw=*pg36&mk%a!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
