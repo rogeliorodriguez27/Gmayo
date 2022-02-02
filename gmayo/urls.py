399b927bf7d193b78b3de8f7b051dfc637af3b52
rogelio <rogelio23rodriguez@gmail.com>
2022-02-01 21:32:08 -0400

    make it better

--- gmayo/urls.py
+++ gmayo/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/dashboard')),
     
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
