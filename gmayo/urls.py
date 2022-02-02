3cae28741cbaeeb81c5307b19e6c42bd2a398c2a
rogelio <rogelio23rodriguez@gmail.com>
2022-02-01 21:01:34 -0400

    make it better

--- gmayo/urls.py
+++ gmayo/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("report/", include("components.reportes.urls")),
     path('', RedirectView.as_view(url='/dashboard')),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
